8ea0892ac3bc11984ec605db555018f46cded321
Paul Starodub <amorallex@gmail.com>
2023-06-03 17:56:17 +0300

    Updated connect.py

--- connect.py
+++ connect.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, text
 
 
-engine = create_engine("sqlite:///sample.db")
+engine = create_engine("sqlite:///sample.db", echo=True)  # echo show sql query
 
 with engine.connect() as connection:
-    result = connection.execute(text("select 'Hello'"))
-
-    print(result.all())
+    pass
+    # result = connection.execute(text("select 'Hello'"))
+    # print(result.all())
